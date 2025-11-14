637df47e085d1f94e10c8b9dca6846cd58d73236
juannico654 <jnicolascl7@gmail.com>
2025-11-14 09:55:30 -0500

    Add files via upload

---
+++ config/reserva_controlador.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../modelo/mesas.php";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id_mesa = $_POST["id_mesa"];
+    $fecha = $_POST["fecha"];
+    $personas = $_POST["personas"];
+    $nombre = $_POST["nombre"];
+    $telefono = $_POST["telefono"];
+    $email = $_POST["email"];
+    $preferencias = $_POST["preferencias"];
+
+    $reserva = new Reserva();
+    $reserva->agregarReserva($id_mesa, $fecha, $personas, $nombre, $telefono, $email, $preferencias);
+
+    header("Location: ../HTML/reservas.html?ok=1");
+    exit();
+}
+?>
